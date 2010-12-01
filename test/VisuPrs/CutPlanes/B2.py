8a307c5c134fa96d1b1c1c5858f056be3d643509
vsv <vsv@opencascade.com>
2010-12-01 12:37:51 +0000

    *** empty log message ***

--- test/VisuPrs/CutPlanes/B2.py
+++ test/VisuPrs/CutPlanes/B2.py
@@ -8,7 +8,7 @@ import paravis
 # Create presentations
 myParavis = paravis.myParavis
 
-file = datadir +  "cube_hexa8_quad4.med"
+file = datadir +  "cube_hexa8_quad4_236.med"
 print " --------------------------------- "
 print "file ", file
 print " --------------------------------- "
