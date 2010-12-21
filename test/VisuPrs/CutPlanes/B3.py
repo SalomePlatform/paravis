b88b95ee763cd793d008143b1c9a01011105b27a
mzn <mzn@opencascade.com>
2010-12-21 15:04:58 +0000

    Add new case.

---
+++ test/VisuPrs/CutPlanes/B3.py
@@ -0,0 +1,38 @@
+# This case corresponds to: /visu/CutPlanes/B3 case
+
+from paravistest import datadir
+from presentations import CutPlanesOnField, EntityType
+import paravis
+import pvsimple
+
+my_paravis = paravis.myParavis
+
+#====================Stage1: Importing MED file====================
+print "**** Stage1: Importing MED file"
+
+print 'Import "ResOK_0000_236.med"...............',
+
+file_path = datadir + "ResOK_0000_236.med"
+my_paravis.ImportFile(file_path)
+med_reader = pvsimple.GetActiveSource()
+
+if med_reader is None:
+    print "FAILED"
+else:
+    print "OK"
+
+#====================Stage2: Creating CutPlanes====================
+print "*****Stage2: Creating CutPlanes"
+
+print "Creating Cut Planes.......",
+
+node_entity = EntityType.NODE
+field_name = 'vitesse'
+cutplanes = CutPlanesOnField(med_reader, node_entity, field_name, 1)
+
+if cutplanes is None :
+    print "FAILED"
+else :
+    print "OK"
+
+
