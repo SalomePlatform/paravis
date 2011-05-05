bd72e6207b7c752aeff391f57fcd957bbba0a432
ploix <ploix>
2011-05-05 15:01:18 +0000

    modify the modes macro so that the vector field is scaled during the animation

--- src/Macro/modes.py
+++ src/Macro/modes.py
@@ -2,44 +2,73 @@ try: paraview.simple
 except: from paraview.simple import *
 paraview.simple._DisableFirstRenderCameraReset()
 
-src = GetActiveSource()
-src_rep = GetRepresentation(src)
-src_rep.Visibility = 0
+source = GetActiveSource()
+representation = GetDisplayProperties(source)
+representation.Visibility = 0
 
-surface = ExtractSurface()
+if source.SMProxy.GetVTKClassName() == 'vtkMedReader' :
+  source.AnimationMode = 'Modes'
 
-warp = WarpByVector()
-warp_repr = Show()
+ExtractSurface1 = ExtractSurface()
+ScaleVector1 = ScaleVector()
+WarpByVector1 = WarpByVector()
 
-info = surface.GetDataInformation()
+# first, find the first point-centered vector array.
+ExtractSurface1.UpdatePipeline()
+info = ExtractSurface1.GetDataInformation()
+pinfo = info.DataInformation.GetPointDataInformation()
+arrayinfo = None
+vectorname = None
+rootname = None
+for arrayid in range(0, pinfo.GetNumberOfArrays()) :
+  arrayinfo = pinfo.GetArrayInformation(arrayid)
+  if arrayinfo.GetNumberOfComponents() == 3 : 
+    vectorname = arrayinfo.GetName()
+    rootpos = vectorname.rfind("[")
+    if rootpos == -1 :     
+      rootname = vectorname
+    else :
+      rootname = vectorname[0:rootpos-1]
+    ScaleVector1.VectorField = ['POINTS', vectorname]
+    WarpByVector1.Vectors = ['POINTS', rootname]
+    break
+
+ScaleVector1.ScaleFactor = 0
+ScaleVector1.UpdatePipeline()
 bounds = info.DataInformation.GetBounds()
-side = [bounds[1] - bounds[0], bounds[3]-bounds[2], bounds[5]-bounds[4]]
-len = max(max(side[0], side[1]), side[2])
+side = [bounds[1] - bounds[0], bounds[3] - bounds[2], bounds[5] - bounds[4]]
+length = side[0]
+if side[1] > length : length = side[1] 
+if side[2] > length : length = side[2] 
 
-scale_factor = len / 20.0
+scale = length / 20
+if vectorname != None :
+  arrayrange = arrayinfo.GetComponentRange(-1)
+  if arrayrange[1] > 0 :
+    scale = scale / arrayrange[1]
 
-scene = GetAnimationScene()
+WarpByVector1.ScaleFactor = scale
 
-PI = 3.14159
-scene.PlayMode = 'Sequence'
-scene.StartTime = -PI
-scene.EndTime = PI
-scene.NumberOfFrames = 20
-scene.Loop = 1
-time_cue = scene.Cues[0]
-time_cue.Enabled = 0
+AnimationScene1 = GetAnimationScene()
 
-track = GetAnimationTrack("ScaleFactor")
-keyf0 = CompositeKeyFrame()
-keyf0.Interpolation='Sinusoid'
-keyf0.KeyTime = 0
-keyf0.KeyValues = [scale_factor]
+TimeAnimationCue1 = GetTimeTrack()
+TimeAnimationCue1.Enabled = 0
 
-keyf1 = CompositeKeyFrame()
-keyf1.Interpolation='Sinusoid'
-keyf1.KeyTime = 1
-keyf1.KeyValues = [scale_factor]
+KeyFrameAnimationCue1 = GetAnimationTrack( ScaleVector1.GetProperty('ScaleFactor'))
+KeyFrame0 = CompositeKeyFrame( KeyValues=[1.0], Interpolation='Sinusoid' )
+KeyFrame1 = CompositeKeyFrame( KeyTime=1.000000001, KeyValues=[1.0] )
 
-track.KeyFrames = [keyf0, keyf1]
+KeyFrameAnimationCue1.KeyFrames = [ KeyFrame0, KeyFrame1 ]
 
-scene.Play()
+AnimationScene1.Cues.append(KeyFrameAnimationCue1)
+AnimationScene1.Loop = 1
+AnimationScene1.PlayMode = 'Sequence'
+AnimationScene1.NumberOfFrames = 21
+
+WarpByVectorDataRepresentation = Show(WarpByVector1)
+if rootname != None :
+  pvLookupTable = GetLookupTableForArray( rootname, 3, VectorMode='Magnitude' )
+  WarpByVectorDataRepresentation.ColorArrayName = rootname
+  WarpByVectorDataRepresentation.LookupTable = pvLookupTable
+
+AnimationScene1.Play()
