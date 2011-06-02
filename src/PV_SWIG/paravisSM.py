fa27e5dd8943ebe6cbb39befbc792eba201116c4
vsr <vsr@opencascade.com>
2011-06-02 13:23:15 +0000

    Update copyright

--- src/PV_SWIG/paravisSM.py
+++ src/PV_SWIG/paravisSM.py
@@ -1,3 +1,22 @@
+# Copyright (C) 2010-2011  CEA/DEN, EDF R&D
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+#
+# See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
+#
+
 r"""servermanager is a module for using paraview server manager in Python.
 One can always use the server manager API directly. However, this module
 provides an interface easier to use from Python by wrapping several VTK
