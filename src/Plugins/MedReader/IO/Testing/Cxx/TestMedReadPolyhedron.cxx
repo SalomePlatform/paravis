428452683a438b50ddeb34c130e61c38f87d6fac
vsr <vsr@opencascade.com>
2012-04-13 13:07:09 +0000

    Update copyright information

--- src/Plugins/MedReader/IO/Testing/Cxx/TestMedReadPolyhedron.cxx
+++ src/Plugins/MedReader/IO/Testing/Cxx/TestMedReadPolyhedron.cxx
@@ -1,19 +1,21 @@
-/*  This file is part of MED.
- *
- *  COPYRIGHT (C) 1999 - 2010  EDF R&D, CEA/DEN
- *  MED is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Lesser General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  MED is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with MED.  If not, see <http://www.gnu.org/licenses/>.
- */
+// Copyright (C) 2010-2012  CEA/DEN, EDF R&D
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+//
+// See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
+//
 
 /*
  *  How to create an unstructured mesh with polygons
